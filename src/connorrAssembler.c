b89c54a7900c5494af56c97b2323e5e638ae0d23
Connor F. Reed <46138604+Blank2275@users.noreply.github.com>
2025-09-12 13:29:33 -0600

    Part 2

--- src/connorrAssembler.c
+++ src/connorrAssembler.c
@@ -1,9 +1,9 @@
 // name: Connor Reed
 // CSC2025 Assembler Project
-// date: 8/29/25
-// i/o files: part1CR.asm
+// date: 9/12/25
+// i/o files: part2CR.asm
 // description: Reads an assembly file and generates machine code then executes that machine code on a virtual machine
-// to read just one part of a line
+// currently implemented: Mov with registers and constants, halt
 
 #define _CRT_SECURE_NO_WARNINGS  // lets us use deprecated code
 
@@ -12,7 +12,7 @@
 #include <string.h>
 #include <ctype.h>
 
-char ASM_FILE_NAME[ ] = "part1CR.asm";
+char ASM_FILE_NAME[ ] = "part2CR.asm";
 
 #define MAX 150			// strlen of simulators memory can be changed
 #define COL 7			// number of columns for output
@@ -48,7 +48,7 @@ struct Registers
 //GLOBAL VARIABLES
 typedef short int Memory;  // sets the type of memory to short int
 Memory memory[MAX] = { 0 };   // global variable the memory of the virtual machine
-Memory address;     // global variable the current address in the virtual machine
+Memory address;     // global variable the current addres s in the virtual machine
 
 //function prototypes
 void runMachineCode( );	// Executes the machine code	****NEEDS WORK***
@@ -59,18 +59,19 @@ void printMemoryDump( );	// Prints memory with commands represented as integers
 
 // Helper functions prototypes
 int convertToNumber( char line[ ], int start );	// converts a sub-string to an int
-int whichOperand( char operand[]);			// Returns the number of the letter register
+int whichOpperand( char operand[]);			// Returns the number of the letter register
 void changeToLowerCase( char line[ ] );	// Changes each character to lower case
 void printMemoryDumpHex( );				// Prints memory in hexadecimal
 void putValue( int operand, int value );
 Memory getValue( Memory operand );
 void readInstructionPart(char line[], char part[], int *index);
-
+Memory readRegister(int reg);
+void writeRegister(int reg, Memory value);
 int main( )
 {
 	assembler( );
 	runMachineCode( );
-	printMemoryDump( );  //displays memory with final values
+	printMemoryDumpHex( );  //displays memory with final values
 	
 	printf( "\n" );
 	system( "pause" );
@@ -112,13 +113,15 @@ void convertToMachineCode( FILE *fin )
 	char part1[LINE_SIZE];	// the asm command
 	char part2[ LINE_SIZE ] = "";// the two operands, could be empty
 	char part3[ LINE_SIZE ] = "";	
-	int machineCode = 0;			// One line of converted asm code from the file
+	Memory machineCode = 0;			// One line of converted asm code from the file
 
 	fgets( line, LINE_SIZE, fin );		// Takes one line from the asm file
 	changeToLowerCase( line );
 	
 	splitCommand( line, part1, part2, part3 );
 
+		// determines whether it will read in a value to put in the 16 bit slot
+	int requires_16_bit_field = 0; // 1 if 16 bits are required at end of command for a constant or memory address
 	if ( part1[0] == 'h' )  //halt
 	{
 		memory[address] = HALT;
@@ -126,13 +129,29 @@ void convertToMachineCode( FILE *fin )
 	}
 	else if ( part1[0] == 'm' )  //move into a register
 	{
-		//put the command into the first 3 bits of machineCode
-		//put the first operand (register) into the next 2 bits (use bitshift)
-		//put the second operand into the last 3 bits 
+			// 3 bits for op, 2 bits for reg1, 3 bits for reg2 or constant
+
+			// operand 1 is not the command but the first operand to follow it
+		int operand1 = whichOpperand(part2); // the first operant of the line
+		int operand2 = whichOpperand(part3); // the second operand of the line
+		machineCode=192;
+		machineCode = machineCode | operand1 << 3; // bit shifts 3 to the left and adds it to machine code
+		machineCode = machineCode | operand2;
+		memory[address] = machineCode;
+
+		requires_16_bit_field = operand2 == CONSTANT; // we need the 16 bit field if we have a constant
+		address++;
 	}
+
+	if (requires_16_bit_field) { // we need to record the 16 bits after this command
+			// convert part3 to a number and store it in the next memory slot
+		memory[address] = (Memory) convertToNumber(part3, 0);
+		address += 1;
+	}
+
 	     //output memory, for debugging, comment out when you don't need it. could use printMemoryDumpHex
-	printf( "\n" );
-	printMemoryDump( );
+	// printf( "\n" );
+	// printMemoryDumpHex( );
 }
 
 
@@ -167,7 +186,7 @@ void splitCommand( char line[ ], char instruction[ ], char operand1[ ], char ope
 		}
 	}
 	//for debugging, comment out when you don't need it
-	printf( "\nCommand = %s %s %s", instruction, operand1, operand2 );
+	// printf( "\nCommand = %s %s %s", instruction, operand1, operand2 );
 }
 
 /********************   runMachineCode   ***********************
@@ -188,18 +207,26 @@ void runMachineCode( )
 	address++;
 	while ( fullCommand != HALT )
 	{
+		// parts of the command
 		part1 = fullCommand & mask1;
 		part2 = fullCommand & mask2;
 		part3 = fullCommand & mask3;
 		if ( part1 == MOVREG )
 		{
-			//get the value from part3
-			//put the value into the register specified by part2
+			Memory value = 0; // the value to put into the target register
+			if (part3 == CONSTANT) {
+				value = memory[address]; // fetch memory
+				address ++; // step over memory
+			} else {
+				value = readRegister(part3);
+			}
+			int target_register = part2 >> 3;
+			writeRegister(target_register, value);
 		}
 		fullCommand = memory[ address ];  //the next command
-		address++;
+		address ++; // next command
 		//debugging, comment out when you don't need it
-		printMemoryDump( );
+		// printMemoryDumpHex( );
 	}
 }
 
@@ -246,7 +273,7 @@ void printMemoryDump( )
 	for ( int row = 0; row < numRows; row++ )
 	{
 		location = row;
-			for ( int column = 0; location < MAX&&column < COL; column++ )
+			for ( int column = 0; location < MAX && column < COL; column++ )
 		{
 			if ( !(numRows - 1 == row&&carryOver - 1 < column) )
 			{
@@ -300,12 +327,61 @@ void printMemoryDumpHex( )
 /*********** helper function for converting to machine code ******************/
 /*****************************************************************************/
 
-/*********************   whichOperand   ***************************
+/*
+ * Reads the register with bit value of reg ex: reg=000 for AX
+ * You pass in the register and it returns the value stored there as Memory
+ *
+ * This function will fatally error if you pass in a nonexistent register
+ */
+Memory readRegister(int reg) {
+	switch ( reg ) { // read the value of either the register or constant specified
+		case AXREG:
+			return (Memory) regis.AX;
+		case BXREG:
+			return (Memory) regis.BX;
+		case CXREG:
+			return (Memory) regis.CX;
+		case DXREG:
+			return (Memory) regis.DX;
+		default: // nonexistent register
+			printf("Unknown register: %d", reg);
+			system("pause");
+			exit(1);
+	}
+}
+
+/*
+ * Writes a value of type Memory into the register specified by reg
+ * reg is the binary value of the register you want to target, 000 for AX, 001 for BX etc.
+ * This function will give a fatal error if you pass in a bad register
+ */
+void writeRegister(int reg, Memory value) {
+	switch (reg) { // move value into the proper register
+		case AXREG:
+			regis.AX = value;
+			break;
+		case BXREG:
+			regis.BX = value;
+			break;
+		case CXREG:
+			regis.CX = value;
+			break;
+		case DXREG:
+			regis.DX = value;
+			break;
+		default: // if the machine code tells it to put it into a non-existent register
+			printf("Error, register %d not recognized", reg);
+			system("pause");
+			exit(1);
+	}
+}
+
+/*********************   whichOpperand   ***************************
 /* changes the letter of the register to a number parameters:
 /* letter - the first letter of the operand, register, number, [
 /* return value - the number of the register
 /*--------------------------------------------------------------*/
-int whichOperand( char operand[LINE_SIZE] )
+int whichOpperand( char operand[LINE_SIZE] )
 {
 	char letter = operand[ 0 ];
 	if ( letter == 'a' )
@@ -385,4 +461,6 @@ void changeToLowerCase( char line[ ] )
 
 /* Problems:
 > Part 1: None
+> Part 1: I had to do some debugging when abstracting the reading and
+writing of registers into their own functions, I have it figured out now.
 */
