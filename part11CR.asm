c9c40db17d15489b2195eeb35ec159ce6defdeba
Connor F. Reed <46138604+Blank2275@users.noreply.github.com>
2025-11-29 09:43:26 -0700

    Searching done, got rid of print

--- part11CR.asm
+++ part11CR.asm
@@ -1,14 +1,4 @@
 ; A = location of where new nodes are kept in memory
-; R = location of the root
-; B = first outer if in insert
-; C = second outer if in insert
-; D = first inner if in insert
-; E = second inner if in insert
-; F = check for null root in insert
-; G = used for calling functions with runtime parameters
-; H = used for calling functions with runtime parameters
-; I = variable used to pass numbers to insert element
-; J = First if statement in print tree
 ;
 ; A node struct has the following structure
 ; Node {
@@ -17,34 +7,43 @@
 ; right
 ; }
 ; I will be using -1 as a null pointer
+; I always pass by reference since both functions are recursive and need runtime arguments
+;
+; DX is the value to insert into the tree
+; insert 10, creates root node
 MOV DX 10
 MOV [I] DX
 FUN [60] 2 [-1] [I]
 MOV BX [9]
 MOV [R] BX
-MOV DX 5
+;insert 15
+MOV DX 15
 MOV [I] DX
 FUN [60] 2 [R] [I]
+; insert 2
 MOV DX 2
 MOV [I] DX
 FUN [60] 2 [R] [I]
+; insert 7
 MOV DX 7
 MOV [I] DX
 FUN [60] 2 [R] [I]
-FUN [180] 1 [R]
+; DX is now the value to search for
+MOV DX 15
+MOV [I] DX
+FUN [180] 2 [I] [R]
+MOV AX [53]
+; Prints 1 if the value is found in the tree, otherwise 0
+PUT
 halt
-
-
-
-
-
-
-
-
 
 
 ; inserts an element into a tree and returns the address
 ; of the inserted element
+; Arguments
+; - 1. the root of the binary tree to insert into, -1 means this will be the root node
+; - 2. the address of the value to insert
+; Return - the value of the node that was just created
 MOV BX [59]
 MOV BX [BX+2]
 MOV AX [BX]
@@ -133,49 +132,68 @@ RET
 
 
 
-; Print tree
+; ===Search Tree===
+; Searches for a value in a binary search tree
+; Args
+; 1. address of value to search for
+; 2. address of root of tree
+; Returns 1 if the value was found, 0 otherwise
 MOV BX [179]
 MOV BX [BX+1]
-; with this line, bx contains the address of the struct
+; DX = value to search for
+MOV DX [BX]
+MOV AX DX
+MOV BX [179]
+; BX = address of root
+MOV BX [BX+2]
 MOV BX [BX]
-; AX = struct.value
-MOV AX [BX]
-PUT
-; start of children
-MOV AX -100
-PUT
-MOV AX [BX+1]
-CMP AX -1
-JE J
-; left child is not null, recursively print it
-; G contains the address of the left child
 MOV AX BX
-ADD AX 1
-MOV [G] AX
-FUN [180] 1 [G]
-; if left child is null, just print -1
-JMP K
-J MOV AX -1
-PUT
-K MOV AX [BX+2]
-CMP AX -1
-JE L
-; right child is not null, recursively print it
-MOV AX BX
-ADD AX 2
-MOV [G] AX
-FUN [180] 1 [G]
-; right child is null, just print -1
-JMP M
-L MOV AX -1
-PUT
-; end of children
-M MOV AX -100
-PUT
+; Checks if value to search for = value of root
+CMP DX [BX]
+JNE K
+; value was found, return true
+MOV AX 1
+RET
+K CMP DX [BX]
+JA L
+; value < root.value
+MOV CX -1
+CMP CX [BX+1]
+JE M
+; left node is not null, search left node recursively
+MOV [G] DX
+MOV CX [BX+1]
+MOV [H] CX
+FUN [180] 2 [G] [H]
+; return recursive call, !HARDCODED ADDRESS
+MOV AX [218]
+RET
+; there is no left node, value not found in tree
+M MOV AX 0
+RET
+; L means value > root.value
+L MOV CX -1
+CMP CX [BX+2]
+JE N
+; right node is not null, search right node recursively
+MOV [G] DX
+MOV CX [BX+2]
+MOV [H] CX
+FUN [180] 2 [G] [H]
+; return recursive call, !HARDCODED ADDRESS
+MOV AX [242]
+RET
+; N means there is no right node so value is not found
+N MOV AX 0
 RET
 
+; Location of the root
 R
+; Used to call functions with runtime arguments
 G
+; Used to call functions with runtime arguments
 H
+; Used to call functions with arguments in the main program
 I
+; Address that structs will be created at, updated each time a new struct is made
 A 300
