6811c3ba35c88a285c2071a1ae73bca4969d4649
Connor F. Reed <46138604+Blank2275@users.noreply.github.com>
2025-11-29 08:50:48 -0700

    Semi Broken Print tree

--- part11CR.asm
+++ part11CR.asm
@@ -8,6 +8,7 @@
 ; G = used for calling functions with runtime parameters
 ; H = used for calling functions with runtime parameters
 ; I = variable used to pass numbers to insert element
+; J = First if statement in print tree
 ;
 ; A node struct has the following structure
 ; Node {
@@ -18,19 +19,25 @@
 ; I will be using -1 as a null pointer
 MOV DX 10
 MOV [I] DX
-FUN [50] 2 [-1] [I]
+FUN [60] 2 [-1] [I]
 MOV BX [9]
 MOV [R] BX
 MOV DX 5
 MOV [I] DX
-FUN [50] 2 [R] [I]
+FUN [60] 2 [R] [I]
 MOV DX 2
 MOV [I] DX
-FUN [50] 2 [R] [I]
+FUN [60] 2 [R] [I]
 MOV DX 7
 MOV [I] DX
-FUN [50] 2 [R] [I]
+FUN [60] 2 [R] [I]
+FUN [180] 1 [R]
 halt
+
+
+
+
+
 
 
 
@@ -38,7 +45,7 @@ halt
 
 ; inserts an element into a tree and returns the address
 ; of the inserted element
-MOV BX [49]
+MOV BX [59]
 MOV BX [BX+2]
 MOV AX [BX]
 MOV BX [A]
@@ -52,7 +59,7 @@ MOV [BX+2] DX
 MOV CX BX
 ; get the root
 ; BX will now contain the address of the address of the root
-MOV BX [49]
+MOV BX [59]
 MOV BX [BX+1]
 ; if the root is null, skip inserting
 CMP BX -1
@@ -81,7 +88,7 @@ MOV DX [BX]
 MOV AX DX
 MOV [H] DX
 ; insert(root.left, this.value)
-FUN [50] 2 [G] [H]
+FUN [60] 2 [G] [H]
 ; we did not use the struct we created so we decrement [A] to reuse this memory
 MOV DX [A]
 SUB DX 3
@@ -103,7 +110,7 @@ MOV BX CX
 MOV DX [BX]
 MOV [H] DX
 ; insert(root.right, this.value)
-FUN [50] 2 [G] [H]
+FUN [60] 2 [G] [H]
 ; we did not use the struct we created so we decrement [A] to reuse this memory
 MOV DX [A]
 SUB DX 3
@@ -119,12 +126,56 @@ MOV AX [A]
 SUB AX 3
 RET
 
+
+
+
+
+
+
+
+; Print tree
+MOV BX [179]
+MOV BX [BX+1]
+; with this line, bx contains the address of the struct
+MOV BX [BX]
+; AX = struct.value
+MOV AX [BX]
+PUT
+; start of children
+MOV AX -100
+PUT
+MOV AX [BX+1]
+CMP AX -1
+JE J
+; left child is not null, recursively print it
+; G contains the address of the left child
+MOV AX BX
+ADD AX 1
+MOV [G] AX
+FUN [180] 1 [G]
+; if left child is null, just print -1
+JMP K
+J MOV AX -1
+PUT
+K MOV AX [BX+2]
+CMP AX -1
+JE L
+; right child is not null, recursively print it
+MOV AX BX
+ADD AX 2
+MOV [G] AX
+FUN [180] 1 [G]
+; right child is null, just print -1
+JMP M
+L MOV AX -1
+PUT
+; end of children
+M MOV AX -100
+PUT
+RET
+
 R
 G
 H
 I
 A 300
-; 141
-; 143
-; 145
-; 147
